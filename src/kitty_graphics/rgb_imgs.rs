afbc4c938a82d4d2fa49cd635bfa32270bc254b5
Ed Carney <carneyedwardj@gmail.com>
2025-05-26 10:16:56 -0400

    Updates RGB images to use rgb crate for common colors

--- src/kitty_graphics/rgb_imgs.rs
+++ src/kitty_graphics/rgb_imgs.rs
@@ -1,14 +1,15 @@
+use rgb::RGB8;
+
 use super::{
     ctrl_seq::{Action, CtrlSeq, PixelFormat},
     encoding, term_ctrl,
 };
 
-pub fn print_red_square(size: usize) -> Result<(), Box<dyn std::error::Error>> {
-    let mut bytes = Vec::with_capacity(size * size * 3);
-    for _ in 0..(size * size) {
-        let mut red: Vec<u8> = vec![255, 0, 0];
-        bytes.append(&mut red);
-    }
+pub fn print_square(size: usize, color: RGB8) -> Result<(), Box<dyn std::error::Error>> {
+    let bytes: Vec<u8> = (0..(size * size))
+        .flat_map(|_| vec![color.r, color.g, color.b])
+        .collect();
+
     let img_data = encoding::read_bytes_to_b64(&bytes)?;
 
     let width = size as u32;
