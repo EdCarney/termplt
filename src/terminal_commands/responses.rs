784dd6d9ac079a28500b18ead029554483161de8
Ed Carney <carneyedwardj@gmail.com>
2025-05-27 09:47:36 -0400

    Updates to CSI codes

--- src/terminal_commands/responses.rs
+++ src/terminal_commands/responses.rs
@@ -3,10 +3,9 @@ use std::io::{self, Read, Write};
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub fn execute_csi(cmd: &str) -> Result<String> {
-    let resp_start = "\x1b";
-    let resp_end = "R";
-    let cmd = String::from("\x1b[") + cmd;
+pub fn execute_csi(cmd: &str, resp_end: &str) -> Result<String> {
+    let resp_start = "\x1b[";
+    let cmd = String::from(resp_start) + cmd;
     let resp = execute_and_read(&cmd, resp_start, resp_end)?;
     resp_to_str(resp, resp_start, resp_end)
 }
@@ -65,11 +64,14 @@ fn resp_to_str(resp: Vec<u8>, resp_start: &str, resp_end: &str) -> Result<String
 }
 
 pub fn read_command() -> Result<()> {
-    let resp = execute_csi("6n")?;
+    let resp = execute_csi("6n", "R")?;
+    println!("CSI Resp: {resp:?}");
+
+    let resp = execute_csi("c", "c")?;
     println!("CSI Resp: {resp:?}");
 
     let resp = execute_kitty("i=31,s=1,v=1,a=q,d=t,f=24;AAAA")?;
     println!("Kitty Resp: {resp:?}");
+
     Ok(())
 }
-
