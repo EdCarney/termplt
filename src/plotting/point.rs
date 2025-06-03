04c15c95ba1e97b1599e861a89419c177bcfdd42
Ed Carney <carneyedwardj@gmail.com>
2025-06-03 18:44:49 -0400

    Adds more point tests

--- src/plotting/point.rs
+++ src/plotting/point.rs
@@ -86,12 +86,52 @@ mod tests {
     }
 
     #[test]
-    fn subtract_point_to_point() {
+    fn subtract_point_from_point() {
         let p1 = Point { x: 10.0, y: 15.5 };
         let p2 = Point { x: 5.5, y: 7.5 };
 
         let p3 = p1 - p2;
         assert_eq!(p3.x, 4.5);
         assert_eq!(p3.y, 8.0);
+    }
+
+    #[test]
+    fn add_f32_to_point() {
+        let p1 = Point { x: 10.0, y: 15.5 };
+        let x = 15.5;
+
+        let p2 = p1 + x;
+        assert_eq!(p2.x, 25.5);
+        assert_eq!(p2.y, 31.0);
+    }
+
+    #[test]
+    fn subtract_f32_from_point() {
+        let p1 = Point { x: 10.0, y: 15.5 };
+        let x = 15.5;
+
+        let p2 = p1 - x;
+        assert_eq!(p2.x, -5.5);
+        assert_eq!(p2.y, 0.0);
+    }
+
+    #[test]
+    fn multiply_point_by_f32() {
+        let p1 = Point { x: 10.0, y: 15.5 };
+        let x = 3.0;
+
+        let p2 = p1 * x;
+        assert_eq!(p2.x, 30.0);
+        assert_eq!(p2.y, 46.5);
+    }
+
+    #[test]
+    fn divide_point_by_f32() {
+        let p1 = Point { x: 10.0, y: 15.0 };
+        let x = 5.0;
+
+        let p2 = p1 / x;
+        assert_eq!(p2.x, 2.0);
+        assert_eq!(p2.y, 3.0);
     }
 }
