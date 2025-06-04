d65018d1d991049c6d6585e20915b61d0837ca91
Ed Carney <carneyedwardj@gmail.com>
2025-06-04 10:06:48 -0400

    Updates markers definition

--- src/plotting/markers.rs
+++ src/plotting/markers.rs
@@ -1,5 +1,17 @@
 use super::colors;
+use crate::common::Result;
+use std::ops::Range;
 use rgb::RGB8;
+
+struct CanvasPoint { x: u32, y: u32 }
+struct CanvasRange { x_range: Range<u32>, y_range: Range<u32> }
+
+pub trait Canvas {
+    fn draw_point(point: CanvasPoint, color: RGB8) -> Result<()>;
+    fn draw_range(range: CanvasRange, color: RGB8) -> Result<()>;
+    fn draw_marker(&self, at: CanvasPoint, style: MarkerStyle) -> Result<()>;
+    fn draw_line(&self, between: CanvasRange, style: LineStyle) -> Result<()>;
+}
 
 #[derive(Debug, PartialEq)]
 pub enum MarkerStyle {
